13e7ca3de7ee9da874bfd7d50a4a5c8155db7b4a
afrilelo111-stack <julieelysia907@gmail.com>
2026-06-08 20:12:36 +0800

    menambahkan SEO

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,9 +1,8 @@
-export function robots() {
+export default function robots() {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin", "/private"],
     },
     sitemap: "https://pplg-gacor.vercel.app/sitemap.xml",
   };
